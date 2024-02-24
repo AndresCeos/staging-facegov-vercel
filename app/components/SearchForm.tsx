7160368713ee4ac99a112e03397671f74187e5cd
Juan Martinez <66894611+JuanMtx20@users.noreply.github.com>
2024-02-24 11:25:17 -0600

    Update SearchForm.tsx

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -31,7 +31,7 @@ function SearchForm() {
           href={`/search/?query=${search}`}
           className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
         >
-          Search
+          Buscar
         </Link>
       </div>
     </form>
