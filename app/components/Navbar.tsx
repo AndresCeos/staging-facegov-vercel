583c235f1cbc58ca0e9b908b13a5416f67de7bea
Juan Martinez <66894611+JuanMtx20@users.noreply.github.com>
2024-02-27 23:16:14 -0600

    add menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import Link from 'next/link';
+
+import { HiBars4 } from 'react-icons/hi2';
+
 import Button from './Button';
 import SearchForm from './SearchForm';
 import UserDropdown from './UserDropDown';
@@ -14,7 +17,7 @@ function Navbar() {
             <h1 className="w-full text-3xl">FACESGOV</h1>
           </Link>
         </div>
-        <div className="grow flex items-center gap-10 justify-end">
+        <div className="hidden md:flex grow items-center gap-10 justify-end">
           <Link className="text-sm text-gray-500 hover:text-gray-700" href="/tendencias">Tendencias</Link>
           <Link className="text-sm text-gray-500 hover:text-gray-700" href="/contacto">Contacto</Link>
           <SearchForm />
@@ -32,6 +35,15 @@ function Navbar() {
           </Button>
 
           <UserDropdown />
+        </div>
+        <div className="grid place-items-center md:hidden">
+          <button
+            type="button"
+            onClick={() => console.log('Menu')}
+          >
+            <span className="sr-only">Menú</span>
+            <HiBars4 className="text-3xl" />
+          </button>
         </div>
       </div>
     </header>
