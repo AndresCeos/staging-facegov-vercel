ea4be0ab1d61125ed85dc5d9eabe4d6e12dcaef0
Juan Martinez <66894611+JuanMtx20@users.noreply.github.com>
2024-04-02 21:39:04 -0600

    add missing pages

---
+++ app/contacto/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <main>
+      <h2 className="text-5xl">Contacto</h2>
+    </main>
+  );
+}
+
+export default Page;
---
+++ app/tendencias/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <main>
+      <h2 className="text-5xl">Tendencias</h2>
+    </main>
+  );
+}
+
+export default Page;
