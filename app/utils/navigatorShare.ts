3068bdcfc0e1300787ad7134cadb2459599a7cd2
Juan Martinez <66894611+JuanMtx20@users.noreply.github.com>
2024-04-08 20:29:17 -0600

    fix image name

--- app/utils/navigatorShare.ts
+++ app/utils/navigatorShare.ts
@@ -2,14 +2,15 @@ const navigatorShare = async (politicalFigure: Api.PoliticalFigure, comment?: Ap
   const imageUrl = new URL(`/political-figures/${politicalFigure.slug}/nav-share${comment?.id ? `${comment.id}/share-image` : ''}`, process.env.API_URL).toString();
   const fetchedImage = await fetch(imageUrl);
   const blobImage = await fetchedImage.blob();
+  const fileImage = `${politicalFigure.firstName} ${politicalFigure.lastName}.png`;
   const filesArray = [
-    new File([blobImage], `${politicalFigure.slug || 'image'}.png`, {
+    new File([blobImage], fileImage, {
       type: 'image/png',
       lastModified: Date.now(),
     }),
   ];
   const shareData = {
-    title: `${politicalFigure.firstName} ${politicalFigure.lastName}`,
+    title: fileImage,
     files: filesArray,
     url: document.location.origin,
   };
