5e63c83ec6cd4af61f364357c38d7ee23d2d92a6
AndresCeos <139495871+AndresCeos@users.noreply.github.com>
2024-09-10 12:09:04 -0700

    Feat/ails/change urls (#52)
    * feat: wip
    
    * feat: change urls
    
    ---------
    
    Co-authored-by: Juan Martinez <66894611+JuanMtx20@users.noreply.github.com>

--- app/utils/navigatorShare.ts
+++ app/utils/navigatorShare.ts
@@ -1,12 +1,18 @@
+import { Md5 } from 'ts-md5';
+
 const navigatorShare = async (politicalFigure: Api.PoliticalFigure, comment?: Api.Comment) => {
+  const md5CommentId = comment?.id ? Md5.hashStr(comment.id.toString()) : '';
   const politicalFigureUrl = new URL(`/politico/${politicalFigure.slug}`, process.env.APP_URL).toString();
-  const imageUrl = new URL(`/political-figures/${politicalFigure.slug}/${comment?.id ? `comments/${comment.id}/share-image` : 'nav-share'}`, process.env.API_URL).toString();
+  const imageUrl = new URL(
+    `/${politicalFigure.slug}/${comment?.id ? `comment/${md5CommentId}/comment.jpeg` : 'nav/mobile.jpeg'}`,
+    process.env.MEDIA_URL,
+  ).toString();
   const fetchedImage = await fetch(imageUrl);
   const blobImage = await fetchedImage.blob();
   const fileImage = `${politicalFigure.firstName} ${politicalFigure.lastName}.png`;
   const filesArray = [
     new File([blobImage], fileImage, {
-      type: 'image/png',
+      type: 'image/jpeg',
       lastModified: Date.now(),
     }),
   ];
