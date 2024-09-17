3de964ccb2b6e0e5bcea4d16804eaee7ef5ec2de
Juan Martinez <66894611+JuanMtx20@users.noreply.github.com>
2024-09-16 19:28:26 -0600

    fix: urls (#57)
    Co-authored-by: AndresCeos <139495871+AndresCeos@users.noreply.github.com>

--- app/utils/url.ts
+++ app/utils/url.ts
@@ -1,5 +1,11 @@
-const url = (path: string, base: 'api' | 'webapp'): string => {
-  const baseUrl = base === 'api' ? process.env.API_URL : process.env.APP_URL;
+const bases = {
+  api: process.env.API_URL ?? 'https://api.aquiestaelmenu.com',
+  webapp: process.env.APP_URL ?? 'https://aquiestaelmenu.com',
+  media: process.env.MEDIA_URL ?? 'https://media.aquiestaelmenu.com',
+};
+
+const url = (path: string, base: 'api' | 'webapp' | 'media'): string => {
+  const baseUrl = bases[base];
   const urlObject = new URL(path, baseUrl);
   return urlObject.toString();
 };
--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
     APP_URL: process.env.APP_URL,
-  }
-}
+    MEDIA_URL: process.env.MEDIA_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
