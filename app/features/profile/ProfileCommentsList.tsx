141f8d7ed40a8f6e0ca18e54932dfd3fcb1aacf9
AndresCeos <139495871+AndresCeos@users.noreply.github.com>
2024-09-16 18:38:23 -0700

    feat: wip

--- app/features/profile/ProfileCommentsList.tsx
+++ app/features/profile/ProfileCommentsList.tsx
@@ -9,6 +9,7 @@ import { useUserComments } from '@/api/users';
 import QueryResult from '@/components/QueryResult';
 import Share from '@/components/Share';
 import formatNameSlug from '@/utils/formatNameSlug';
+import url from '@/utils/url';
 import PoliticalFigureCommentRating from '../politicalFigures/comments/PoliticalFigureCommentRating';
 import PoliticalFigureCommentUtility from '../politicalFigures/comments/PoliticalFigureCommentUtility';
 
@@ -26,7 +27,7 @@ function ProfileCommentsList() {
           <div className="flex flex-col md:flex-row">
             <div className="flex flex-col items-center mb-5 mx-auto md:border-r-2 md:border-gray-300 w-full max-w-[13rem] md:min-h-64">
               <img
-                src={(comment.politicalFigure.media as any).find((media: { key: string; }) => media?.key === 'featured')?.url ? `${process.env.MEDIA_URL}/${(comment.politicalFigure.media as any).find((media: { key: string; }) => media?.key === 'featured')?.url}` : 'https://placehold.co/375'}
+                src={(comment.politicalFigure.media as any).find((media: { key: string; }) => media?.key === 'featured')?.url ? `${url((comment.politicalFigure.media as any).find((media: { key: string; }) => media?.key === 'featured')?.url, 'media')}` : 'https://placehold.co/375'}
                 alt="profile"
                 className="w-10 md:w-20 h-10 md:h-20 rounded-full object-cover"
               />
--- app/utils/navigatorShare.ts
+++ app/utils/navigatorShare.ts
@@ -1,10 +1,11 @@
 import { Md5 } from 'ts-md5';
+import url from './url';
 
 const navigatorShare = async (politicalFigure: Api.PoliticalFigure, comment?: Api.Comment) => {
   const md5CommentId = comment?.id ? Md5.hashStr(comment.id.toString()) : '';
-  const politicalFigureUrl = new URL(`/politico/${politicalFigure.slug}`, process.env.APP_URL).toString();
+  const politicalFigureUrl = url(`/politico/${politicalFigure.slug}`, 'api');
   const urlString = `${comment?.id ? `comment/${md5CommentId}/comment.jpeg` : 'nav/mobile.jpeg'}`;
-  const imageUrl = new URL(`/${politicalFigure.slug}/${urlString}`, process.env.MEDIA_URL).toString();
+  const imageUrl = url(`/${politicalFigure.slug}/${urlString}`, 'media');
   const fetchedImage = await fetch(imageUrl);
   const blobImage = await fetchedImage.blob();
   const fileImage = `${politicalFigure.firstName} ${politicalFigure.lastName}.jpeg`;
